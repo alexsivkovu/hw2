2b375115dd2cf6f2c496587024c01160b9b33aa5
Aleksandr Sivkov <aasivkov@edu.hse.ru>
2022-06-01 14:48:05 +0300

    ii

--- venv/app.py
+++ venv/app.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 
+app = Flask(__name__)
+
 # make config
 db_config = {}
 for var in ['DB_USER_NAME', 'DB_USER_PASSWORD', 'DB_NAME', 'DB_HOST']:
@@ -17,6 +19,18 @@ for var in ['DB_USER_NAME', 'DB_USER_PASSWORD', 'DB_NAME', 'DB_HOST']:
             var: default_db_conf['DEFAULT_' + var]
         })
 
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'postgresql://%(user_name)s:%(password)s@%(host)s/%(db)s' % ({
+        'user_name': db_config['DB_USER_NAME'],
+        'password': db_config['DB_USER_PASSWORD'],
+        'host': db_config['DB_HOST'],
+        'db': db_config['DB_NAME']
+    })
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'ea2c4ed0-721e-4e3b-985f-3397a2a67837'
+db = SQLAlchemy(app)
+
 
 class Number(db.Model):
     __tablename__ = 'Number'
@@ -24,23 +38,11 @@ class Number(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Integer())
 
-    def __init__(self, value):
-        self.value = value
+
+def __init__(self, value):
+    self.value = value
 
 
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'postgresql://%(user_name)s:%(password)s@%(host)s/%(db)s'%({
-        'user_name': db_config['DB_USER_NAME'],
-        'password': db_config['DB_USER_PASSWORD'],
-        'host': db_config['DB_HOST'],
-        'db': db_config['DB_NAME'],
-    })
-
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = 'ea2c4ed0-721e-4e3b-985f-3397a2a67837'
-db = SQLAlchemy(app)
 db.create_all()
 migrate = Migrate(app, db)
 
@@ -58,7 +60,7 @@ def start_page() -> None:
 
 
 @app.route('/truncate')
-def truncate() -> None:
+def truncate() -> dict[str, Union[int, str]]:
     """
     Truncate nubers table
     :return: if works, returns 200 response
@@ -81,7 +83,7 @@ def nums():
         if request.is_json:
             new_num_value = request.get_json()['number']
             check = [i.value for i in db.session.query(Number).filter(
-                Number.value.in_([new_num_value, new_num_value-1])).all()]
+                Number.value.in_([new_num_value, new_num_value - 1])).all()]
             if not check:
                 new_num = Number(value=request.get_json()['number'])
                 db.session.add(new_num)
